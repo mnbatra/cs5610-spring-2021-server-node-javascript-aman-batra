f1aaec66430ed0d7c2e4ed84a64a5981514c4237
Aman Batra <btaman4@gmail.om>
2021-04-11 17:32:57 -0400

    Moving demo to controllers

---
+++ controllers/demo-controller.js
@@ -0,0 +1,20 @@
+
+module.exports = (app) => {
+
+    const add = (rq,rs) => {
+        const a = parseInt(rq.params['paramA']);
+        const b = parseInt(rq.params['paramB']);
+        rs.send(`${a+b}`);
+    }
+
+    const subtract = (req,res) => {
+        const x = req.query['x'];
+        const y = req.query['y'];
+        res.send(`${x-y}`);
+    }
+
+    app.get('/add/:paramA/:paramB/', add);
+    app.get('/subtract',subtract);
+    app.get('/',(req, res) => res.send('hello world'));
+
+}
