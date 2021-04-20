ca07af907f25938290c1c14eadb3d33979c3ddfa
Aman Batra <btaman4@gmail.om>
2021-04-20 01:21:47 -0400

    added remote

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-require('dotenv').config()
+const dotenv = require("dotenv").config()
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+const password = process.env.PASSWORD
+const username = process.env.USERNAME
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/whiteboard',
-    {useNewUrlParser: true, useUnifiedTopology: true});
-
+const remoteUri = 'mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.a0ii5.mongodb.net/whiteboard?retryWrites=true&w=majority'
+try {
+    mongoose.connect(remoteUri,
+        {useNewUrlParser: true, useUnifiedTopology: true},
+        () => console.log('connected to db'));
+} catch (err) {
+    console.log('failed to connect to db')
+}
 //CONFIGURING CORS
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
